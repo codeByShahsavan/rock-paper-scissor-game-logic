adbb0490447088c390ce6e04559f1aa254e63179
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-01 22:26:11 +0330

    check who winner

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 const choices=["rock","paper","scissor"]
 const playerChoice=prompt("choose rock,paper,scissor")
-console.log(playerChoice)
 
 if(playerChoice){
     console.log(`You choose ${playerChoice}`)
@@ -13,3 +12,25 @@ const randomNumber=Math.floor(Math.random()*choices.length)
 const computerChoice=choices[randomNumber]
 console.log(`computer chooses ${computerChoice}`)
 
+if(playerChoice===computerChoice){
+    console.log("It's a tie.")
+}else if(playerChoice==="rock"){
+      if(computerChoice==="scissors"){
+        console.log("You win.")
+      }else{
+        console.log("You lose!")
+      }
+}else if(playerChoice==="paper"){
+          if(computerChoice==="rock"){
+        console.log("You win.")
+      }else{
+        console.log("You lose!")
+      }
+}
+else{
+        if(computerChoice==="paper"){
+        console.log("You win.")
+      }else{
+        console.log("You lose!")
+      }
+}
