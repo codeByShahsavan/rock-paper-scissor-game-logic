df43638d9a2f5d7f08c7d7b0c73d31a7cc5f30a9
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-02 01:21:59 +0330

    scoring

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const choices = ["rock", "paper", "scissor"];
-
+    let playerScore=0
+    let computerScore=0
 const checkWinner = (player, computer) => {
   if (player === computer) {
     return "draw";
@@ -13,13 +14,20 @@ const checkWinner = (player, computer) => {
 };
 
 const showResult=(result)=>{
+
    if(result==="player"){
     console.log("You win.")
+    playerScore++
    }else if(result==="computer"){
       console.log("You lose!")
+      computerScore++
    }else{
     console.log("It's a tie.")
    }
+   console.log(`Your score:${playerScore}`)
+   console.log(`computer score:${computerScore}`)
+   console.log("................")
+
 }
 const play=()=>{
 const playerChoice = prompt("choose rock,paper,scissor");
@@ -37,6 +45,11 @@ console.log(`computer chooses ${computerChoice}`);
 
 const gameResult = checkWinner(playerChoice, computerChoice);
 showResult(gameResult)
+while(playerScore<5 && computerScore<5){
+
+play()
+
+}
 
 }
 
